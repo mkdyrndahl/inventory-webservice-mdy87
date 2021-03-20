8f31ca970cfb9d2cc0dc1964a676066c7d5dedf2
Matt Dyrndahl <mkdyrndahl@gmail.com>
2021-03-20 13:16:20 -0700

    Created logic for handling all relevant HTTP requests

--- index.js
+++ index.js
@@ -69,22 +69,51 @@ let cart = [
 // The webservice handles all HTTP requests through the express framework. 
 
 // POST
+// Once a POST request is received within the cartMDy87 route, 
 exp.post('/cartMDy87', (req,res) => {
-
+    // Ensure that the quantity is updated if the cartItem exists 
+    const exists = (cartItem) => cartItem.sku == req.body.sku 
+    if(cart.some(exists)){
+        cart.cartItem.quantity++
+        res.send(cart.cartItem)
+    } else {
+        var item = {
+            sku: req.body.sku,
+            name: req.body.name,
+            price: req.body.price,
+            quantity: 1
+        }
+        cart.push(item)
+        res.send(item)
+    }
 })
 
+// If the POST request is received in the checkout route, the cart is processed and the correct number of items in the inventory are removed.
 exp.post('/cartMDy87/checkout', (req, res) => {
-
+    cart.foreach(cartItem => {
+        inventory.foreach(invItem => {
+            if(invItem.sku == cartItem.sku){
+                invItem.quantity = inveItem.quantity - cartItem.quantity
+            }
+        })
+    })
 })
 
 // GET 
+// Once a GET request is received in the inventoryMDy87 route, an updated inventory list is sent to the application.
 exp.get('/inventoryMDy87', (req, res) => {
-
+    res.send(inventory)
 })
 
 exp.get('/cartMDy87', (req, res) => {
-
+    res.send(cart)
 })
 
 // DELETE
+// Once a DELETE request is received the cart will remove the specified item from the cart
+// Might have to change this to -1 quantity at some point
 exp.delete('/CartMDy87', (req, res) => {
+    const sku = req.params.sku
+    cart = cart.filter(item => item.sku != sku)
+    res.send(cart)
+})
